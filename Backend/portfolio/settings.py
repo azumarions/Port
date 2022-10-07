f260d37b905ac7adbdee9ae93178688cc0eef071
azuma <azuma20011224@gmail.com>
2022-10-08 01:31:29 +0900

    hide secret_key

--- Backend/portfolio/settings.py
+++ Backend/portfolio/settings.py
@@ -25,7 +25,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6aj^g@k1r(pfyr*+f!8sre&n5uz1tvg8lvn4hce^za2nrdz=ab'
 os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
