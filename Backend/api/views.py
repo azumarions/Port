82a3ae9956aa9deddc0f2708bf4fc3781afed230
azuma <azuma20011224@gmail.com>
2022-10-10 00:00:26 +0900

    add views.py

--- Backend/api/views.py
+++ Backend/api/views.py
@@ -1,3 +1,40 @@
-from django.shortcuts import render
+from rest_framework import generics, viewsets
+from rest_framework.permissions import AllowAny
+from . import serializers
+from .models import Profile, Animal, Comment
 
-# Create your views here.
+class CreateUserView(generics.CreateAPIView):
+    serializer_class = serializers.UserSerializer
+    permission_classes = (AllowAny,)
+
+
+class Profileview(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = serializers.ProfileSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(userProfile=self.request.user)
+
+
+class MyProfileListView(generics.ListAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = serializers.ProfileSerializer
+    
+    def get_queryset(self):
+        return self.queryset.filter(userProfile=self.request.user)
+
+
+class AnimalViewSet(viewsets.ModelViewSet):
+    queryset = Animal.objects.all()
+    serializer_class = serializers.AnimalSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(aniaml=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = serializers.CommentSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(comment=self.request.user)
